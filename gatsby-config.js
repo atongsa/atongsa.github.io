b509b24362893648b810f6c00a807e928e3272a4
atongsa <atongsa@163.com>
2023-01-09 20:08:55 +0800

    20230109

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `atongsa's earth life`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
