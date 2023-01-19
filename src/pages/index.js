c7682c9eaa57044266b9d0f059d28fa0d668fe56
atongsa <atongsa@163.com>
2023-01-19 17:26:57 +0800

    20230119

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ export const pageQuery = graphql`
           slug
         }
         frontmatter {
-          date(formatString: "MMMM DD, YYYY")
+          date(formatString: "MMMM DD HH:MM, YYYY")
           title
           description
         }
